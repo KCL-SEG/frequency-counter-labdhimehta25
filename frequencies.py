f0caf04e818cc697aa790985f6fe344522ac5e58
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-06 13:57:49 +0100

    Checking file

--- frequencies.py
+++ frequencies.py
@@ -1,10 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+#check file
+
 def frequencies(items):
     frequencies = {}
     for i in items:
         value = items[i]
-        
-        
+
+
     return frequencies
