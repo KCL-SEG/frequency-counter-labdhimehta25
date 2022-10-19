38690869a88d349067e0c822a1b0466e21d80108
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-19 23:38:13 +0100

    Remove variable value

--- frequencies.py
+++ frequencies.py
@@ -6,13 +6,12 @@
 def frequencies(items):
     frequencies = {}
     for i in items:
-        value = items[i]
         #if item is not a string, convert it into one
         if type(i) == int:
             i = str(i)
 
         if i in frequencies:#increment if already dictionary
-            value = value + 1
+            frequencies[i] = frequences.get(i) + 1
         else:#if not in dictionary then create it with the starting value of 1
             frequencies[str(i)]=1
 
